f04e7cbf013070e34e1e07e37b8d13c7d224093b
SoyaChunkz <119126144+SoyaChunkz@users.noreply.github.com>
2024-07-10 03:05:31 +0530

    Update Dashboard.jsx

--- frontend/HashNotes/src/pages/Home/Dashboard.jsx
+++ frontend/HashNotes/src/pages/Home/Dashboard.jsx
@@ -217,7 +217,7 @@ const Dashboard = () => {
           },
         }}
         contentLabel=''
-        className='w-[90%] sm:w-[70%] md:w-[40%] max-h-[80%] bg-white dark:bg-neutral-900 rounded-md mx-auto mt-14 p-4 overflow-y-scroll overscroll-contain'>
+        className='w-[90%] sm:w-[70%] md:w-[40%] max-h-[80%] bg-white dark:bg-neutral-900 rounded-md mx-auto mt-14 p-4 overflow-y-scroll 2xl:overflow-y-hidden overscroll-contain'>
 
 
         <AddEditNotes 
