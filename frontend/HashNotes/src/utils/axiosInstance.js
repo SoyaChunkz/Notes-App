cda7c4c7f225285d12f6c1adb16d0f51b82f74f2
SoyaChunkz <sameermaroof26@gmail.com>
2024-07-12 01:06:05 +0530

    updated axiosInstance.js

--- frontend/HashNotes/src/utils/axiosInstance.js
+++ frontend/HashNotes/src/utils/axiosInstance.js
@@ -7,7 +7,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
     baseURL: process.env.API_BASE_URL,
-    timeout: 10000,
+    timeout: 15000,
     headers: {
         "Content-Type": "application/json"
     },
