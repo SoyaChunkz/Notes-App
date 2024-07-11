a2b156caaf806f572ff8b868a77f3786aaa5cd8a
SoyaChunkz <sameermaroof26@gmail.com>
2024-07-12 01:41:03 +0530

    updated ToGithub.jsx

--- frontend/HashNotes/src/components/ToGithub/ToGithub.jsx
+++ frontend/HashNotes/src/components/ToGithub/ToGithub.jsx
@@ -8,7 +8,7 @@ const ToGithub = ({ darkMode }) => {
 
   return (
     <button
-      className="fixed bottom-28 left-10 bg-gray-100 dark:bg-neutral-700 text-gray-800 dark:text-neutral-300 p-2 rounded-full shadow-xl hover:scale-125 transition-all ease-in-out z-50"
+      className="fixed bottom-28 left-10 bg-gray-100 dark:bg-neutral-700 dark:text-neutral-300 p-2 rounded-full shadow-xl hover:scale-125 transition-all ease-in-out z-50"
       onClick={handleGitHubRedirect}
       aria-label="GitHub Repository"
     >
